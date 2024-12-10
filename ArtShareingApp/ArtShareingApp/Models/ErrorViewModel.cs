4320dbb7eea18b594ca13d4e86bce847d040dc99
Kaloyan <kaloyan.savchev06@gmail.com>
2024-12-10 11:21:30 +0200

    Initial Commit

---
+++ ArtShareingApp/ArtShareingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtShareingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
