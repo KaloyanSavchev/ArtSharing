edb5d28834164c99545a58b28e40ea04899c6770
Kaloyan <kaloyan.savchev06@gmail.com>
2025-03-04 09:45:48 +0200

    Initial

---
+++ ArtSharing.Web/ArtSharing.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtSharing.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
