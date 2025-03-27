b4af1ffc2ba7d5eb32956fad4c14ffcab9d06372
Kaloyan <kaloyan.savchev06@gmail.com>
2025-03-28 00:07:52 +0200

    added viewmodel

---
+++ ArtSharing.Web/ArtSharing.Web/Models/PostCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArtSharing.Web.Models
+{
+    public class PostCreateViewModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public IFormFile ImageFile { get; set; }
+    }
+}
