37faf1e219f281e6f0a226a0391dfb3aeebc65b7
Kaloyan <kaloyan.savchev06@gmail.com>
2025-04-14 23:09:00 +0300

    dropdown menu for admin

---
+++ ArtSharing.Web/ArtSharing.Web/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtSharing.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult ManageModerators()
+        {
+            return View();
+        }
+
+        public IActionResult ManageCategories()
+        {
+            return View();
+        }
+
+        public IActionResult Reported()
+        {
+            return View();
+        }
+    }
+}
